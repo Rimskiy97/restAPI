119265f2b7aa4bdcf554fe6f02c12d45f6adfefd
Rim Khabibullin <Rimkhabibullin@gmail.com>
2019-09-12 17:30:01 +0400

    new sout

--- src/test/java/restapi.java
+++ src/test/java/restapi.java
@@ -6,16 +6,19 @@ public class restapi {
         public void track200(){
             RestAssured.given()
                     .when().post("http://httpbin.org/status/200").then().statusCode(200);
+            System.out.println("Test #1 succesful!");
         }
         @Test
         public void track300(){
             RestAssured.given()
                     .when().post("http://httpbin.org/status/300").then().statusCode(300);
+            System.out.println("Test #2 succesful!");
         }
         @Test
         public void track404(){
             RestAssured.given()
                     .when().post("http://httpbin.org/status/300").then().statusCode(404);
+            System.out.println("Test #3 succesful!");
         }
     }
 }
